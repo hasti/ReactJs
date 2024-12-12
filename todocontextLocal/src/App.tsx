4a0d557464a8e554320aa1d09ea12f107b4bfbc4
Hastimal Shah <hasti@hastishah.com>
2024-12-12 22:48:29 +0530

    Initialize React + TypeScript + Vite project with Tailwind CSS and ESLint configuration

---
+++ todocontextLocal/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline m-auto text-center">
+        Hello World! 
+      </h1>
+      
+    </>
+  )
+}
+
+export default App
